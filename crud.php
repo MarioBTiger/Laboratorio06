efb5c3c016780650cfbcd9702351f20801bcf28c
MarioBTiger <85888524+MarioBTiger@users.noreply.github.com>
2022-04-21 23:27:23 -0500

    Update crud.php

--- crud.php
+++ crud.php
@@ -1,7 +1,6 @@
 <?php
 include_once 'db.php';
 
-
 /* code for data insert */
 if(isset($_POST['save']))
 {
@@ -9,7 +8,7 @@ if(isset($_POST['save']))
     $ln = $MySQLiconn->real_escape_string($_POST['ln']);
 
     $SQL = $MySQLiconn-> query("INSERT INTO data(fn, ln) 
-        VALUES('$fn','$ln')");
+        VALUES('$fn ','$ln')");
     
 
     if(!$SQL)
@@ -33,17 +32,14 @@ if(isset($_GET['del']))
 /* code for data update*/ 
 if(isset($_GET['edit']))
 {
-    $SQL = $MySQLiconn->query("SELECT * FROM data WHERE id
-        =".$_GET['edit']);
+    $SQL = $MySQLiconn->query("SELECT * FROM data WHERE id=".$_GET['edit']);
     $getROW = $SQL-> fetch_array();
 }
 if(isset($_POST['update']))
 {
-    $SQL = $MySQLiconn->query("UPDATE data SET fn='".$_
-        POST['fn']."', ln='".$_POST['ln']."' WHERE id=".$_
-        GET['edit']);   
+    $SQL = $MySQLiconn->query("UPDATE data SET fn='".$_POST['fn']."', ln='".$_POST['ln']."' WHERE id=".$_GET['edit']);   
     header("Location: index.php");
-}
+}   
 /* code for data update*/ 
 
 ?>
